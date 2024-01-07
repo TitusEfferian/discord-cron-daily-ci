ab30a55676213934f676a4c606b418fa12f639fd
titusefferian <kiritosato@gmail.com>
2024-01-07 18:50:05 +0900

    handle dev

--- main.js
+++ main.js
@@ -59,7 +59,7 @@ client.once(Events.ClientReady, async readyClient => {
     context.fillStyle = 'white';
     const keepItUpText = `今日も頑張りましょう。`;
     const keepItUpTextX = canvas.width / 2;
-    const keepItUpTextY = rectY + rectHeight + 52;
+    const keepItUpTextY = textY + 24;
 
     // Draw the text
     context.fillText(keepItUpText, keepItUpTextX, keepItUpTextY);

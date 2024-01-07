ef8062f77a64dcecf0cdea4ddd5843cb864e6558
titusefferian <kiritosato@gmail.com>
2024-01-07 15:53:44 +0900

    handle dev

--- main.js
+++ main.js
@@ -1,2 +1,23 @@
-console.log('testinng');
-console.log(process.env.DISCORD_TOKEN);
+// Require the necessary discord.js classes
+const { Client, Events, GatewayIntentBits } = require('discord.js');
+
+// Create a new client instance
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+const DEV_CHANNEL = process.env.DISCORD_BOT_DEV_CHANNEL;
+const token = process.env.DISCORD_TOKEN;
+
+// When the client is ready, run this code (only once).
+// The distinction between `client: Client<boolean>` and `readyClient: Client<true>` is important for TypeScript developers.
+// It makes some properties non-nullable.
+client.once(Events.ClientReady, async readyClient => {
+    readyClient.channels.cache.get(DEV_CHANNEL).send('hello world');
+   
+    /**
+     * how to end the operation and make this code exit?
+     */
+    client.destroy();
+});
+
+// Log in to Discord with your client's token
+client.login(token);

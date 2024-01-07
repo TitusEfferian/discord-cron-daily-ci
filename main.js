50741e8ad7b04304e40c13d6107f9321d910b2f9
titusefferian <kiritosato@gmail.com>
2024-01-07 17:52:12 +0900

    handle dev

--- main.js
+++ main.js
@@ -8,18 +8,18 @@ const DEV_CHANNEL = process.env.DISCORD_BOT_DEV_CHANNEL;
 const token = process.env.DISCORD_TOKEN;
 
 client.once(Events.ClientReady, async readyClient => {
-    const canvas = Canvas.createCanvas(200, 200);
-    const context = canvas.getContext('2d');
+    // const canvas = Canvas.createCanvas(200, 200);
+    // const context = canvas.getContext('2d');
 
-    // Draw a square
-    context.fillStyle = 'blue'; // Square color
-    context.fillRect(50, 50, 100, 100); // x, y, width, height
+    // // Draw a square
+    // context.fillStyle = 'blue'; // Square color
+    // context.fillRect(50, 50, 100, 100); // x, y, width, height
 
-    // Convert canvas to buffer
-    const buffer = canvas.toBuffer();
+    // // Convert canvas to buffer
+    // const buffer = canvas.toBuffer();
 
-    // Create an attachment and send it
-    const attachment = new AttachmentBuilder(buffer, { name: 'square.png' });
+    // // Create an attachment and send it
+    // const attachment = new AttachmentBuilder(buffer, { name: 'square.png' });
 
     readyClient.channels.cache.get(DEV_CHANNEL).send("hello world again");
     client.destroy();
